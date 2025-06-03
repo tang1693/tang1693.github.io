b624935b219cd162487cdd16c3ee6d99591ac380
Yang Tang <85510879+tang1693@users.noreply.github.com>
2025-06-03 15:03:25 -0400

    Refine site layout

--- script.js
+++ script.js
@@ -17,5 +17,12 @@ document.addEventListener('DOMContentLoaded', function () {
             backSpeed: 25,
             loop: true
         });
+
+        new Typed('#abstract-typed', {
+            strings: ['Yang Tang is a Ph.D. candidate specializing in geospatial data analysis and remote sensing. This site showcases his research contributions, publications, and professional experiences.'],
+            typeSpeed: 40,
+            backSpeed: 20,
+            showCursor: false
+        });
     }
 });
